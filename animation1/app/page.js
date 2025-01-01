202f67390697ff2b5d2430c62045064f23db0a30
webwizzz <jatin.tomar@somaiya.edu>
2025-01-01 17:13:21 +0530

    css done

---
+++ animation1/app/page.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from "./page.module.scss"
+
+const page = () => {
+  return (
+    <main className={styles.main} >
+        <div className={styles.mask}>
+          hi i am jatin tomar im a exprert 
+          fullstck developer doint development 
+          from 1 year from my college and created 
+          lot of cool designer websites 
+        </div>
+
+        <div className={styles.body}>
+         my name is jatin tomar i have just completed 
+          fullstck development earlier i was focusing 
+          on frontend development from 1year from my 
+          college and created shit websites 
+        
+        </div>
+    </main>
+  )
+}
+
+export default page
